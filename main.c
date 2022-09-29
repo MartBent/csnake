cd90b6f91a08b667d596365f79962697a71fd3d4
MartBent <martbent99@gmail.com>
2022-09-29 10:55:38 +0200

    Moved portable snake lib to seperate folder, added main file with example. finished driver struct

--- main.c
+++ main.c
@@ -1,31 +1,14 @@
-#include <stdio.h>
-#include <stdbool.h>  
 #include <time.h>
 #include <stdlib.h>
 #include <errno.h>
-#include <string.h>
+#include <stdio.h>
 
-#include "types.h"
+#include "portable-snake/snake.h"
 
-//Since the screen is 1024*1024 the width of the rectangles should be 32 pixels. 
-#define RES 128 //Resolution should always be square, and a multiple of 32
-#define SIZE 32 //Game will always be 32 tiles
-#define SQUARE_SIZE RES/SIZE //Adjust the square sizes to the resolution ratio
-#define SQUARE_DIAMETER SQUARE_SIZE/2
-
-typedef u8 framebuffer_t[RES][RES];
-
-//Extern
-int usleep(long msec)
+void delay(unsigned long msec)
 {
     struct timespec ts;
     int res;
-
-    if (msec < 0)
-    {
-        errno = EINVAL;
-        return -1;
-    }
 
     ts.tv_sec = msec / 1000;
     ts.tv_nsec = (msec % 1000) * 1000000;
@@ -33,144 +16,46 @@ int usleep(long msec)
     do {
         res = nanosleep(&ts, &ts);
     } while (res && errno == EINTR);
-
-    return res;
 }
 
-//Extern
-direction_t read_direction() {
+void display_flush(const framebuffer_t* grid, const unsigned long resolution) {
     
-}
-
-//Extern
-void flush_framebuffer(const framebuffer_t* buffer) {
-
-}
-
-void clear_buffer(framebuffer_t* buffer) {
-    
-    for(u8 x = 0; x < RES; x++) {
-        for(u8 y = 0; y < RES; y++) {
-            *buffer[x][y] = 32;
-        }
+    for (u8 i = 0; i < resolution; i++) {
+        printf("-");
     }
-}
-
-void print_buffer(const framebuffer_t* grid) {
-    
-    for (u8 i = 0; i < RES; i++) {
-        for (u8 j = 0; j < RES; j++) {
+    printf("\n");
+    for (u8 i = 0; i < resolution; i++) {
+        printf("|");
+        for (u8 j = 0; j < resolution; j++) {
             printf("%c", *grid[j][i]);
         }
+        printf("|");
         printf("\n");
     }
-}
-
-void draw_square(framebuffer_t* buffer, point_t location) {
-    //Location is valid
-    if(location.x < SIZE && location.y < SIZE) {
-        for(u8 x = 0; x < RES; x++) {
-            for(u8 y = 0; y < RES; y++) {
-                if(is_between(location.x*SQUARE_SIZE - SQUARE_DIAMETER, location.x*SQUARE_SIZE  + SQUARE_DIAMETER, x) && 
-                   is_between(location.y*SQUARE_SIZE - SQUARE_DIAMETER, location.y*SQUARE_SIZE  + SQUARE_DIAMETER, y)) {
-                    *buffer[x][y] = 88;
-                }
-            }
-        }
+    for (u8 i = 0; i < resolution; i++) {
+        printf("-");
     }
+    printf("\n");
+}
+u8 rnd() {
+    u8 res = rand() % 32;
+
+    printf("%d", res);
+    return res;
+}
+direction_t read_direction() {
+    return down;
 }
 
-bool detect_collision_snake(const snake_t* snake) {
-    for(int i = 1; i < snake->length; i++) {
-        if(point_t_equals(snake->current_location, snake->point_history[i])) {
-            return true;
-        }
-    }
-    return false;
-}
 
-void move_snake(snake_t* snake) {
-    switch(snake->current_direction) {
-        case down: {
-            snake->current_location.y = snake->current_location.y == SIZE ? 1 : snake->current_location.y + 1;
-            break;
-        }
-        case up: {
-            snake->current_location.y = snake->current_location.y == 0 ? SIZE-1 : snake->current_location.y - 1;
-            break;
-        }
-        case right: {
-            snake->current_location.x = snake->current_location.x == SIZE ? 1 : snake->current_location.x + 1;
-            break;
-        }
-        case left: {
-            snake->current_location.x = snake->current_location.x == 0 ? SIZE-1 : snake->current_location.x - 1;
-            break;
-        }
-    }
-
-
-    memmove(&snake->point_history[1], &snake->point_history[0], (19)*sizeof(point_t));
-    snake->point_history[0] = snake->current_location;
-}
-
-void game_play() {
-
-    snake_t snake = {
-        .current_direction = right,
-        .current_location = {1,31},
-        .length = 1,
-        .point_history = {}
-    };
-    snake.point_history[0] = snake.current_location;
-
-    point_t current_food = {
-        31,
-        31
-    };
-
-    framebuffer_t* frame_buffer = malloc(sizeof(framebuffer_t[RES][RES]));
-    if(frame_buffer == NULL) {
-        printf("Malloc failed!");
-    }
-
-    while(1) {
-
-        flush_framebuffer(frame_buffer);
-
-        clear_buffer(frame_buffer);
-
-        for(int i = 0; i < snake.length; i++) {
-            draw_square(frame_buffer, snake.point_history[i]);
-        }
-
-        draw_square(frame_buffer, current_food);
-
-        print_buffer(frame_buffer);
-      
-        //Move snake
-        move_snake(&snake);
-        printf("Location: X: %d, Y: %d\n", snake.current_location.x, snake.current_location.y);
-        //Detect collision with food / snake
-        if(detect_collision_snake(&snake)) {
-
-            //Dead
-            printf("Dead\n");
-            usleep(10000);  
-        }
-
-        if(point_t_equals(snake.current_location, current_food)) {
-            snake.length += 1;
-            
-            current_food.x = rand() % SIZE;
-            current_food.y = rand() % SIZE;
-        }
-
-        //Process any food
-        usleep(100);
-    }   
-}
 int main() {
     srand(time(NULL));
-    game_play();
+    snake_driver_t driver;
+    driver.delay_function_cb = delay;
+    driver.display_flush_cb = display_flush;
+    driver.random_number_cb = rnd;
+    driver.read_direction_cb = read_direction;
+    driver.resolution = 128;
+    driver.snake_length = 33;
+    snake_play(&driver);
 }   
